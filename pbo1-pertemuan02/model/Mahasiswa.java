dc977d1ddd1e2a2f8419ed8495cd19076cc60a42
musagd12 <62414491+musagd12@users.noreply.github.com>
2020-03-24 16:58:32 +0800

    Add files via upload

---
+++ pbo1-pertemuan02/model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa{
+	public static void tampilanInfo(){
+		System.out.println();
+		System.out.println("== Data Mahasiswa=========");
+		System.out.println("NPM : 18630984");
+		System.out.println("Nama : Alfi Reza Rahimi");
+		System.out.println("==========================");
+	}
+}
